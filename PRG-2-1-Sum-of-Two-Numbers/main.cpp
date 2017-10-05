ef94428229f87c12bb3adf2fb7f775f2c8ad22ca
Keith Smith <aeiouandsometimesy@me.com>
2017-10-05 18:16:33 -0400

    Initial Commit

---
+++ PRG-2-1-Sum-of-Two-Numbers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PRG-2-1-Sum-of-Two-Numbers
+//
+//  Created by Keith Smith on 10/5/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
